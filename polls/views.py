995bcccc12caa6426ec834ae6ccd6e41338a9b8b
Andre <[andre_w3@yahoo.com]>
2018-10-23 18:05:25 -0700

    python poll admin section

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 
 
 def index(request):
-	return HttpResponse("Hello, world. your at polls index. polls/view.")
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	# template = loader.get_template('polls/index.html')
+	context = {'latest_question_list': latest_question_list}
+	# output = ', '.join([q.question_text for q in latest_question_list])
+	# return HttpResponse(template.render(context, request))
+	return render(request, 'polls/index.html', context)
+	# return HttpResponse("Hello, world. your at polls index. polls/view.")
 
 # Create your views here.
+def detail(request, question_id):
+	return HttpResponse("you're looking at question %s." % question_id)
+
+def results(request, question_id):
+	response = "You're looking at results of question %s."
+	return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+	return HttpResponse("You're voting on question %s." % question_id)
